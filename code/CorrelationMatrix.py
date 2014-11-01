b10a12b19c15d04ce0d22e40b31c41517297a95e
Yamini Gotimukul <yamini485@gmail.com>
2014-10-31 19:05:46 -0500

    cleaned the code

--- code/CorrelationMatrix.py
+++ code/CorrelationMatrix.py
@@ -3,8 +3,6 @@ import numpy as np
 from scipy import stats as st
 from ItemBasedRecommendation import get_training_data
 
-
-
 #   Initializing the matrix to all zeros for No of users = 49290 items = 139739
 s = (49290,139739)
 num_users=49290
@@ -30,6 +28,7 @@ def get_user_input_matrix():
 #Generating the rating matrix
 get_user_input_matrix()
 
+#calculates average rating for each user and stores in the user_avg_rating_matrix
 def user_averages():
     global rating_matrix
     global user_avg_rating_matrix
@@ -39,6 +38,7 @@ def user_averages():
             user_avg_rating_matrix[i]=np.sum(rating_matrix[i])*1.0/nne
     print user_avg_rating_matrix
 
+# Takes as input i,j which are the indices for users and returns the pearson coefficient for item i and j
 def pearson_corr(i,j):
     global rating_matrix
     global user_avg_rating_matrix
@@ -57,33 +57,14 @@ def pearson_corr(i,j):
     if (deno !=0):
         print num/deno
 
-# Takes as input i,j which are the indices for users and returns the common set of items rated both i and j
-def get_pearson_coeff(i,j):
-    global rating_matrix
-    global pearson_correlation_matrix
-    col1 = rating_matrix[:,i]
-    col2 = rating_matrix[:,j]
-    subset = (col1>0)&(col2>0) ## returing the boolean values
-    rate_i = rating_matrix[subset,i]
-    rate_j = rating_matrix[subset,j]
-    if len(rate_i)!=0:
-        pearson_correlation_matrix[i][j]=st.pearsonr(rate_i,rate_j)[0]
-#common_items = rating_matrix[[[i],[j]],subset1]
-#return(common_items) # returns two rows , first row rating of user 1 and second rating of user2 for common items
+# ideal suppose to calculate the pearson coefficient matrix
 
 def get_Pearson_Correlation_Matrix():
-    #global pearson_correlation_matrix
-    #get_user_input_matrix()
-    #print rating_matrix
-    for item in range(100):
+    for item in range(100): ## calculating for 100 users
         for item1 in range(item+1,100):
             pearson_corr(item,item1)
 
-
-
-#print temp[0]
-#pearson_correlation_matrix[user][user1]=st.pearsonr(temp[0],temp[1])
-#print pearson_correlation_matrix[0][1]
 user_averages()
 get_Pearson_Correlation_Matrix()
-#np.count_nonzero(pearson_correlation_matrix)
+
+
