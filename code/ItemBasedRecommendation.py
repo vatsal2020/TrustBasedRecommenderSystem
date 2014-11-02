041b5aec82399b6b6cc339231387a925ace44ad5
Yamini Gotimukul <yamini485@gmail.com>
2014-11-02 16:42:02 -0600

    Changed the code for generating test and training data

--- code/ItemBasedRecommendation.py
+++ code/ItemBasedRecommendation.py
@@ -2,60 +2,30 @@ from ReservoirSample import get_random
 import numpy as np
 
 # Sample the user ratings from the Input files
+data = []
+rawdata=np.genfromtxt("data/ratings_data.txt",delimiter=' ',dtype=None, names=True)
+size = (664823,3)
+data= np.zeros(size)
+p=0;
+for i in range(664823):
+    data[i][0]=rawdata[i][0]
+    data[i][1]=rawdata[i][1]
+    data[i][2]=rawdata[i][2]
+size = (581720,1)
+sampled_user_item_data = np.zeros(size)
+size = (581720,3)
+sample_user_item_training =np.zeros(size)
+sampled_user_item_data = np.array(get_random(664823,581720))
+ind=np.zeros(664823)
+ind[sampled_user_item_data]=1
+size_test = (83103,3)
+sample_user_item_test =np.zeros(size_test)
+sample_user_item_training=data[ind==1]
+sample_user_item_test=data[ind==0]
+print sample_user_item_training[0]
+print sample_user_item_test[0]
 
-
-def get_training_data():
-    data = []
-
-    rawdata=np.genfromtxt("data/ratings_data.txt",delimiter=' ',dtype=None, names=True)
-
-#print np.shape(rawdata)[0]
-
-    size = (664823,3)
-    data= np.zeros(size)
-    p=0;
-    for i in range(664823):
-        data[i][0]=rawdata[i][0]
-        data[i][1]=rawdata[i][1]
-        data[i][2]=rawdata[i][2]
-
-#   print data[1:10]
-
-    size = (581720,1)
-    sampled_user_item_data = np.zeros(size)
-    size = (581720,3)
-    sample_user_item_training =np.zeros(size)
-#Sampling the input data using reservoir sampling n = 664824 , k = 581721
-    sampled_user_item_data = np.array(get_random(664823,581720))
-
-#print np.shape(sampled_user_item_data)[0]
-
-    for i in  range(581720):
-        sample_user_item_training[i][0] = data[sampled_user_item_data[i]][0]
-        sample_user_item_training[i][1] = data[sampled_user_item_data[i]][1]
-        sample_user_item_training[i][2] = data[sampled_user_item_data[i]][2]
-#   print sample_user_item_training[1:10]
-
-    return sample_user_item_training
-
-
-
-# Unncomment this for getting the test data
-#size=(83103,3)
-#p=0
-#sample_user_item_test =np.zeros(size)
-#sampled_user_item_data_sorted = np.sort(sampled_user_item_data)
-
-#j=0
-#for i in  range(664823):
-#    if (i == sampled_user_item_data_sorted[j]):
-#j=j+1
-        #    else:
-        #        sample_user_item_test[p][0] = data[i][0]
-        #sample_user_item_test[p][1] = data[i][1]
-        #sample_user_item_test[p][2] = data[i][2]
-#p =p+1
-#print sample_user_item_test[0]
-
-
+# Saving the test data and the training and the test into npy files
+np.save("training",sample_user_item_training)
+np.save("test",sample_user_item_test)
 
