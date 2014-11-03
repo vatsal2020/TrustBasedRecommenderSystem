32b3bb6c5b1d1b614593fef0244670c94e10b0cb
vatsal2020 <vatsalshah1106@gmail.com>
2014-11-02 17:11:25 -0700

    Trust with RMSE

--- code/TrustMatrix.py
+++ code/TrustMatrix.py
@@ -4,6 +4,9 @@ from scipy import stats as st
 from ItemBasedRecommendation import get_training_data
 from TrustDataPoll import get_training_trust_data
 from numpy import random
+import math
+
+
 #import rand 
 #import timeit
 #   Initializing the matrix to all zeros for No of users = 49290 items = 139739
@@ -19,17 +22,20 @@ def get_user_trust_matrix():
     global trust_matrix, num_users
     data = np.load("trust_network.npy")
     size =(num_users, num_users)
-    user_item_matrix= np.zeros(size)
+    rating_matrix= np.zeros(size)
     for j in range(data.shape[0]):
         trust_matrix[data[j][0]][data[j][1]]=data[j][2]
     return trust_matrix
 
 #Retrieve the rating matrix 
 trust_matrix=get_user_trust_matrix()
+np.save('trust.npy', trust_matrix)
+
+
 rating_matrix = get_training_trust_data("sample_user_item_training.txt")
 
 #Function which returns the trust value
-def get_trust(user,item):
+def get_rating_trust(user,item):
     global rating_matrix, trust_matrix
     depth = 0
     r_prob = 1         
@@ -50,13 +56,31 @@ def get_trust(user,item):
             #Probability with which user_j was picked
             r_prob = 1/total_trust
             #Update the numerator and the denominator to find the avg rating of the item i
-            if user_item_matrix[user_j][i] > 0:
-                sum_rating = sum_rating + user_item_matrix[user_j][i]*r_prob
+            if rating_matrix[user_j][i] > 0:
+                sum_rating = sum_rating + rating_matrix[user_j][i]*r_prob
                 den_prob = den_prob + r_prob
                 break
             
     avg_rating = sum_rating/den_prob
     return(avg_rating)
+
+test_data = np.load('test.npy')
+def RMSE(test_data):
+    global test_data
+    RMSE_Trust= 0;
+    # Calculates the error in rating contributed by each (user, item) pair in the test data
+    for i in range(0,np.shape(test_data)[0])
+        user = test_data[i][1]
+        item = test_data[i][2]
+        pred_rating = get_rating_trust(user,item)
+        rating = test_data[i][3]
+        RMSE_Trust = RMSE_Trust + (pred_rating-rating)**2
+
+    RMSE_Trust = math.sqrt(RMSE_Trust/np.shape(test_data)[0])
+    return RMSE_Trust
+
+RMSE_Trust_val = RMSE(test_data)
+            
 
         
             
