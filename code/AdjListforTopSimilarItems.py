d82c1cd94146175a61a7b6d9d621dd35c7a426ef
Yamini Gotimukul <yamini485@gmail.com>
2014-11-23 14:34:43 -0600

    Adding RMSE code

--- code/AdjListforTopSimilarItems.py
+++ code/AdjListforTopSimilarItems.py
@@ -1,16 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 20 17:23:12 2014
-
-@author: Divya
-"""
-
 import numpy as np
 from copy import deepcopy
 
-data=np.genfromtxt("data/trimmed_items_ratings.txt",delimiter=' ',dtype=int)
-data.shape
-data=data[range(5000),:]
+data=np.genfromtxt("data/trimmed_training.txt",delimiter=' ',dtype=int)
+#data=data[range(50000),:]
+
+testdata=np.genfromtxt("data/trimmed_test.txt",delimiter=' ',dtype=int)
+#testdata=testdata[range(5000),:]
+
 
 item_adj_list=dict.fromkeys(data[:,1],None)
 
@@ -27,6 +23,14 @@ for i in user_adj_list.keys():
 
 for i in range(data.shape[0]):
     user_adj_list[data[i][0]].append((data[i][1],data[i][2]))
+    
+user_adj_list_test=dict.fromkeys(testdata[:,0],None)
+
+for i in user_adj_list_test.keys():
+    user_adj_list_test[i]=[]
+
+for i in range(testdata.shape[0]):
+    user_adj_list_test[testdata[i][0]].append((testdata[i][1],testdata[i][2]))    
     
 user_averages=dict.fromkeys(data[:,0],0)
 
@@ -54,22 +58,17 @@ def pearson_corr(i,j):
     for l in range(len(commonusers)):
        useriratings.append(item_adj_list[i][userini.index(commonusers[l])][1]-user_averages[commonusers[l]])
        userjratings.append(item_adj_list[j][userinj.index(commonusers[l])][1]-user_averages[commonusers[l]])
-    #print(useriratings)
-    #print(userjratings)
     num=np.dot(useriratings,userjratings)
     deno1=np.sqrt(np.dot(useriratings,useriratings))
     deno2=np.sqrt(np.dot(userjratings,userjratings))
     deno = deno1*deno2
     if (deno !=0):
-        #print( num*1.0/deno)
         return ( num*1.0/deno)
     else:
         return -2
       
 simij=pearson_corr(i,j)
 print(simij)
-
-# calculate similarity list for all items
 top_similar_items =dict.fromkeys(data[:,1],None)
 
 for i in top_similar_items.keys():
@@ -90,16 +89,13 @@ for i in item_keys:
                     if (similarity>=minsim):
                         top_similar_items[i][minindex]=(j,similarity)
                         
-maxnum=0 
-keyval=0                
-for i in top_similar_items.keys():
-    tempnum=len(top_similar_items[i])
-    if (tempnum>maxnum):
-        maxnum=tempnum
-        keyval=i
-                  
-user=1
-item=keyval                  
+               
+
+        
+
+
+# In[24]:
+
 def predictrating(user,item):
     similarratings=[]
     itemrated=list(zip(*user_adj_list[user]))[0]
@@ -108,8 +104,39 @@ def predictrating(user,item):
             similarratings.append((top_similar_items[item][i][1],user_adj_list[user][itemrated.index(top_similar_items[item][i][0])][1]))
         except IndexError:
             continue
-    print(len(similarratings))
+        except ValueError: # need to check 
+            continue
+    if(len(similarratings)<=2):
+        return -1
     return np.dot(list(zip(*similarratings))[0],list(zip(*similarratings))[1])/sum(list(zip(*similarratings))[0])
+
+
+print(predictrating(1,61))
+
+numpreds=0
+sumerr=0
+for user in user_adj_list_test.keys():
+    if (user_adj_list.has_key(user)):
+        for j in range(len(user_adj_list_test[user])):
+            item=user_adj_list_test[user][j][0]
+            if item_adj_list.has_key(item):
+                pr=predictrating(user,item)
+                if(pr!=-1):
+                    temp = (pr-user_adj_list_test[user][j][1])
+                    sumerr=sumerr+(temp * temp)
+                    numpreds=numpreds+1
+            else:
+                continue
+    else:
+        continue
         
-print(predictrating(user,item))  
+            
+RMSE=np.sqrt(sumerr/numpreds) 
+
+
+print RMSE
+
+
+
+
 
