170a765bb090cd6181cc62c828d2ca9a176da3a8
sindhu maiyya <sindhumaiyya@wireless-10-145-200-122.public.utexas.edu>
2014-12-05 16:43:51 -0600

    correct CF for users

--- code/CFforUsers.py
+++ code/CFforUsers.py
@@ -1,15 +1,15 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 import numpy as np
 from copy import deepcopy
 
-data=np.genfromtxt("data/trimmed_training.txt",delimiter=' ',dtype=int)
+data=np.genfromtxt("trimmed_training_10_10.txt",delimiter=' ',dtype=int)
 #data=data[range(50000),:]
 
-testdata=np.genfromtxt("data/trimmed_test.txt",delimiter=' ',dtype=int)
+testdata=np.genfromtxt("trimmed_test_10_10.txt",delimiter=' ',dtype=int)
 #testdata=testdata[range(5000),:]
 
 item_adj_list=dict.fromkeys(data[:,1],None)
@@ -21,14 +21,6 @@ for i in item_adj_list.keys():
 for i in range(data.shape[0]):
     item_adj_list[data[i][1]].append((data[i][0],data[i][2]))
     
-item_adj_list_test=dict.fromkeys(testdata[:,1],None)
-
-for i in item_adj_list_test.keys():
-    item_adj_list_test[i]=[]
-
-for i in range(testdata.shape[0]):
-    item_adj_list_test[testdata[i][1]].append((testdata[i][0],testdata[i][2]))    
-    
 user_adj_list=dict.fromkeys(data[:,0],None)
 
 for i in user_adj_list.keys():
@@ -36,14 +28,6 @@ for i in user_adj_list.keys():
 
 for i in range(data.shape[0]):
     user_adj_list[data[i][0]].append((data[i][1],data[i][2]))
-    
-user_adj_list_test=dict.fromkeys(testdata[:,0],None)
-
-for i in user_adj_list_test.keys():
-    user_adj_list_test[i]=[]
-
-for i in range(testdata.shape[0]):
-    user_adj_list_test[testdata[i][0]].append((testdata[i][1],testdata[i][2]))    
     
 user_averages=dict.fromkeys(data[:,0],0)
 
@@ -53,10 +37,30 @@ for i in user_averages.keys():
 
 # In[2]:
 
+no_of_Test= 7500
+item_adj_list_test=dict.fromkeys(testdata[0:no_of_Test,1],None)
+
+for i in item_adj_list_test.keys():
+   item_adj_list_test[i]=[]
+
+for i in range(no_of_Test):
+   item_adj_list_test[testdata[i][1]].append((testdata[i][0],testdata[i][2])) 
+
+user_adj_list_test=dict.fromkeys(testdata[0:no_of_Test,0],None)
+
+for i in user_adj_list_test.keys():
+    user_adj_list_test[i]=[]
+
+for i in range(no_of_Test):
+    user_adj_list_test[testdata[i][0]].append((testdata[i][1],testdata[i][2]))    
+
+
+# In[2]:
+
 from IPython.core.debugger import Tracer
 
 
-# In[9]:
+# In[3]:
 
 import math
 
@@ -106,7 +110,7 @@ def pearson_corr_user(i,j):
     
 
 
-# In[11]:
+# In[8]:
 
 def predictrating(user,item):
     similarusers=[]
@@ -116,27 +120,17 @@ def predictrating(user,item):
     for j in userrated:
         corrs.append(pearson_corr_user(user,j))
         usr_avg_local.append(user_averages[j])
-    if(len(corrs)<2):
-        return -1
     posit_vals = np.array(corrs) >0
-    neget_vals = (np.array(corrs) <0)&(np.array(corrs)>-2)
+    #neget_vals = (np.array(corrs) <0)&(np.array(corrs)>-2)
     posit_corrs = np.array(corrs)[posit_vals]
-    neget_corrs = np.array(corrs)[neget_vals]
+    #neget_corrs = np.array(corrs)[neget_vals]
     rel_rating = np.array(list(zip(*item_adj_list[item]))[1])-usr_avg_local
-    if(len(posit_corrs)+len(neget_corrs)<=2):
+    if(len(posit_corrs)<=5):
         return -1
-    return user_averages[user] - (np.dot(posit_corrs, rel_rating[posit_vals]) + 
-     np.dot(neget_corrs,6 - rel_rating[neget_vals]))/(sum(posit_corrs) + sum(abs(neget_corrs)))
+    return user_averages[user] + (np.dot(posit_corrs, rel_rating[posit_vals]))/sum(posit_corrs)
 
 
-    
-    
-    
-    
-    
-
-
-# In[ ]:
+# In[9]:
 
 # RMSE calculation
 ##REgular function
@@ -147,7 +141,7 @@ for user in user_adj_list_test.keys():
         for j in range(len(user_adj_list_test[user])):
             item=user_adj_list_test[user][j][0]
             if item_adj_list.has_key(item):
-                pr=predictrating(user,item)
+                pr= predictrating(user,item)
                 if(pr!=-1):
                     temp = (pr-user_adj_list_test[user][j][1])
                     sumerr=sumerr+(temp * temp)
@@ -163,15 +157,14 @@ RMSE=np.sqrt(sumerr/numpreds)
 
 
 print RMSE
-
+print numpreds/7500.0
 
 print len(item_adj_list.keys())
-print len(top_similar_items.keys())
 
 
-# In[6]:
+# In[12]:
 
-
+numpreds
 
 
 # In[5]:
@@ -181,5 +174,5 @@ print len(top_similar_items.keys())
 
 # In[ ]:
 
-
+print RMSE
 
